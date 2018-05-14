521535de6911c48c34df1b96b51e0ed365d03142
Łukasz Kobylski <lukasz@kobylski.net>
2018-05-14 17:43:10 +0200

    Main funcianlity

--- main.go
+++ main.go
@@ -1,7 +1,38 @@
 package main
 
 import "fmt"
+import (
+	"os"
+	"time"
+	"strconv"
+)
+
+//args
+var (
+	timeIn int64
+)
 
 func main() {
-    fmt.Println("Hello, World")
+
+	if len(os.Args) < 2{
+		printUsage()
+	}
+
+	timeIn, err := strconv.ParseInt(os.Args[1], 10, 64)
+	if err != nil {
+		printUsage()
+	}
+
+	t := time.Unix(timeIn, 0)
+	fmt.Println(t)
+
+	os.Exit(1)
+
+}
+
+
+func printUsage() {
+	fmt.Printf("Usage: %s [timestamp]\n", os.Args[0])
+	fmt.Print("timestamp must be integer\n", )
+	os.Exit(1)
 }
